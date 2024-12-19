78559d49e4bd542a58e187d371b8d9c5f794e346
SaadAslam58 <saadxpert58@gmail.com>
2024-12-19 20:44:42 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+   <div className="flex justify-center items-center w-full h-screen ">
+    <div className="grid gap-2 grid-cols-[1fr_2fr_1fr] grid-rows-[1fr_3fr_1fr] w-[90%] h-[90%]">
+      <div className="border-2 transition-all hover:scale-105 p-3 row-span-3 bg-[#FFD900] text-black border-black rounded-md ">
+        <h1 className="text-xl font-bold">Nav</h1>
+      </div>
+      <div className="border-2 transition-all hover:scale-105 p-3 col-span-2 bg-[#FFD900] text-black border-black rounded-md ">
+        <h1 className="text-xl font-bold">Header</h1>
+      </div>
+      <div className="border-2 transition-all hover:scale-105 p-3 bg-[#FFD900] text-black border-black rounded-md ">
+        <h1 className="text-xl font-bold">Article</h1>
+      </div>
+      <div className="border-2 transition-all hover:scale-105 p-3 row-span-2 bg-[#FFD900] text-black border-black rounded-md ">
+        <h1 className="text-xl font-bold">Ads</h1>
+      </div>
+      <div className="border-2 transition-all hover:scale-105 p-3 bg-[#FFD900] text-black border-black rounded-md ">
+        <h1 className="text-xl font-bold">Footer</h1>
+      </div>
+      </div>
+   </div>
+  );
+}
