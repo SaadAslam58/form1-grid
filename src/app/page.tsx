1f1d507763262a50287e8da9c31eef39d840b9db
SaadAslam58 <saadxpert58@gmail.com>
2024-12-20 00:19:22 +0500

    fourth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
    <div className="flex justify-center items-center w-full h-screen ">
-    <div className="grid gap-2 grid-cols-[1fr_2fr_1fr] grid-rows-[1fr_3fr_1fr] w-[90%] h-[90%]">
+    <div className="grid gap-2 grid-cols-[1fr_3fr_1fr] grid-rows-[1fr_4fr_1fr] w-[90%] h-[90%]">
       <div className="border-2 transition-all hover:scale-105 p-3 row-span-3 bg-[#FFD900] text-black border-black rounded-md ">
         <h1 className="text-xl font-bold">Nav</h1>
       </div>
